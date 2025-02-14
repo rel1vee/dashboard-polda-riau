cf05154a0825aa213beed84ee150e19816511e78
rel1vee <muhzakierbaisyas@gmail.com>
2025-02-14 16:48:23 +0700

    Add new company data files for Kampar and Pelalawan; update RiauCity with new company references

---
+++ data/company/Kampar.tsx
@@ -0,0 +1,37 @@
+import { Company } from "@/types";
+
+export const KamparCompanies: Company[] = [
+    {
+      id: 1,
+      name: "KELOMPOK TANI SAWIT HOLI MAKMUR LESTARI",
+      area: 0.00,
+      target2Percent: 0.00,
+      target7Percent: 0.00,
+      monokulturTargets: {
+        I: 0.00,
+        II: 0.00,
+        III: 0.00,
+        IV: 0.00,
+      },
+      monokulturAchievements: {
+        I: 0.00,
+        II: 0.00,
+        III: 0.00,
+        IV: 0.00,
+      },
+      tumpangSariTargets: {
+        I: 0.00,
+        II: 0.00,
+        III: 0.00,
+        IV: 0.00,
+      },
+      tumpangSariAchievements: {
+        I: 0.00,
+        II: 0.00,
+        III: 0.00,
+        IV: 0.00,
+      },
+      status: 0,
+      csr: 0.00,
+    },
+  ];
---
+++ data/company/Pelalawan.tsx
@@ -0,0 +1,38 @@
+import { Company } from "@/types";
+
+export const PelalawanCompanies: Company[] = [
+    {
+      id: 1,
+      name: "PT. KARUNIA BANGSA",
+      area: 0.0,
+      target2Percent: 0.0,
+      target7Percent: 0.0,
+      monokulturTargets: {
+        I: 0.0,
+        II: 0.0,
+        III: 0.0,
+        IV: 0.0,
+      },
+      monokulturAchievements: {
+        I: 0.0,
+        II: 0.0,
+        III: 0.0,
+        IV: 0.0,
+      },
+      tumpangSariTargets: {
+        I: 0.0,
+        II: 0.0,
+        III: 0.0,
+        IV: 0.0,
+      },
+      tumpangSariAchievements: {
+        I: 0.0,
+        II: 0.0,
+        III: 0.0,
+        IV: 0.0,
+      },
+      status: 0,
+      csr: 0.0,
+    },
+  ];
+  
