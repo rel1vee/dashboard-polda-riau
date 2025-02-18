2df63e94ef6e97db403a094cb4cf40ddc5a84d0b
rel1vee <muhzakierbaisyas@gmail.com>
2025-02-19 06:32:55 +0700

    Add progress data for Pekanbaru other companies to enhance tracking

--- data/other-company/Pekanbaru.tsx
+++ data/other-company/Pekanbaru.tsx
@@ -23,6 +23,76 @@ export const pekanbaruOtherCompanies: Company[] = [
       III: 0,
       IV: 0,
     },
+    progress: [
+      {
+        id: 1,
+        namaPJ: "RIDWAN YAHYA",
+        nomorTelp: "081249889129",
+        area: 0.12,
+        coordinates: [0.602256, 101.367824],
+        photo: null,
+        monokultur: {
+          targetTanam: {
+            luas: 0.12,
+            persentase: 0.0,
+          },
+          waktuTanam: "06-11-2024",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.12,
+            persentase: 0.0,
+          },
+          keterangan: "LHN PEMKO",
+        },
+        tumpangSari: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+        csr: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+      },
+    ],
   },
   {
     id: 2,
@@ -46,6 +116,76 @@ export const pekanbaruOtherCompanies: Company[] = [
       III: 0,
       IV: 0,
     },
+    progress: [
+      {
+        id: 2,
+        namaPJ: "RIDWAN YAHYA  ",
+        nomorTelp: "081249889129",
+        area: 1.2,
+        coordinates: [0.602256, 101.367824],
+        photo: null,
+        monokultur: {
+          targetTanam: {
+            luas: 1.2,
+            persentase: 0.0,
+          },
+          waktuTanam: "20-11-2024",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 1.2,
+            persentase: 0.0,
+          },
+          keterangan: "LHN PEMKO",
+        },
+        tumpangSari: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+        csr: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+      },
+    ],
   },
   {
     id: 3,
@@ -69,6 +209,76 @@ export const pekanbaruOtherCompanies: Company[] = [
       III: 0,
       IV: 0,
     },
+    progress: [
+      {
+        id: 3,
+        namaPJ: "BOWO",
+        nomorTelp: "081363734124",
+        area: 2.5,
+        // coordinates: "0°27'01.3\"N 101°31'15.6\"E",
+        photo: null,
+        monokultur: {
+          targetTanam: {
+            luas: 2.5,
+            persentase: 0.0,
+          },
+          waktuTanam: "21-01-2025",
+          progresTanam: {
+            luas: 2.5,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "LHN PEMKO",
+        },
+        tumpangSari: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+        csr: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+      },
+    ],
   },
   {
     id: 4,
@@ -92,5 +302,75 @@ export const pekanbaruOtherCompanies: Company[] = [
       III: 0,
       IV: 0,
     },
+    progress: [
+      {
+        id: 4,
+        namaPJ: "ADE DAULAY",
+        nomorTelp: "082153243380",
+        area: 0.25,
+        // coordinates: "0°34'52.8\"N 101°31'54.3\"E",
+        photo: null,
+        monokultur: {
+          targetTanam: {
+            luas: 0.25,
+            persentase: 0.0,
+          },
+          waktuTanam: "03-02-2025",
+          progresTanam: {
+            luas: 0.25,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "LHN MASY",
+        },
+        tumpangSari: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+        csr: {
+          targetTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          waktuTanam: "",
+          progresTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          belumTanam: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          panen: {
+            luas: 0.0,
+            persentase: 0.0,
+          },
+          keterangan: "",
+        },
+      },
+    ],
   },
 ];
